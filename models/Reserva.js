528b0b0c5826201c2eea6d5f3dd7ed5030d4ae85
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-06-21 15:05:23 -0300

    Se creo un modelo de datos para las Reservas

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,66 @@
 // TODO: Crear modelo de datos de Reserva
+
+// Importar Sequelize y configurar la conexión a la base de datos
+const {sequelize, DataTypes} = require('../db');
+
+// Definir el modelo "Cliente"
+const Cliente = sequelize.define('Cliente', {
+  cliente_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dirección: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  correo_electrónico: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  teléfono: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+// Definir el modelo "Reserva"
+const Reserva = sequelize.define('Reserva', {
+  reserva_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  código_reserva: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  fecha_reserva: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
+
+// Establecer la relación entre Cliente y Reserva
+Cliente.hasMany(Reserva, {foreignKey: 'cliente_id'});
+Reserva.belongsTo(Cliente, {foreignKey: 'cliente_id'});
+
+// Sincronizar los modelos con la base de datos
+sequelize
+  .sync()
+  .then(() => {
+    console.log('Los modelos se han sincronizado correctamente.');
+  })
+  .catch((error) => {
+    console.error('Error al sincronizar los modelos:', error);
+  });
