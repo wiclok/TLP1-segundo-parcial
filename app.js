0870b07d8de43f97974fa13d73bda2cbb7cb8b8f
cespedesBrian <cepedesaguilerabrian@gmail.com>
2023-06-21 14:49:59 -0300

    Se descomprimio el .zip, se instalaron las dependecias, se configuro el modo de arranque y se añadio y README.md con los pasos para instalar y ejecutar el proyecto

---
+++ app.js
@@ -0,0 +1,22 @@
+// Imports
+const cors = require('cors');
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+// Middlewares
+// TODO: Implementar middlewares
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api', require('./routes/reserva.routes'));
+
+// TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+
+// Starting the server
+
+const PUERTO = 45635;
+app.listen(PUERTO, () => console.log(`Server on port ${PUERTO}`));
